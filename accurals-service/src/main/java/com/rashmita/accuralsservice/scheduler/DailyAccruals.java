8d78c5f90d6b3703059e7d8e54951b71cb9fb0a7
rashuuuuuu <rashmitasubedi45@gmail.com>
2025-10-27 09:57:11 +0545

    crate daily scheduler log

--- accurals-service/src/main/java/com/rashmita/accuralsservice/scheduler/DailyAccruals.java
+++ accurals-service/src/main/java/com/rashmita/accuralsservice/scheduler/DailyAccruals.java
@@ -1,7 +1,5 @@
 package com.rashmita.accuralsservice.scheduler;
-
 import com.rashmita.accuralsservice.service.AccuralsService;
-import com.rashmita.accuralsservice.service.AccuralsServiceImpl.TotalPayableImpl;
 import com.rashmita.commoncommon.entity.DailyJobLog;
 import com.rashmita.commoncommon.entity.EmiSchedule;
 import com.rashmita.commoncommon.entity.LoanDetails;
@@ -12,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -20,33 +17,33 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class DailyAccurals {
-
+public class DailyAccruals {
     private final LoanDetailsRepository loanRepo;
     private final EmiScheduleRepository emiRepo;
     private final AccuralsService accuralsService;
-    private final TotalPayableImpl totalPayable;
     private final DailyJobLogRepository jobLogRepository;
-
     private static final int DAYS_IN_YEAR = 365;
-
-        @Scheduled(fixedRate = 60000)
-//    @Scheduled(cron = "0 10 2 * * ?", zone = "Asia/Kathmandu")
+//    @Scheduled(fixedRate = 60000)
+//    @Scheduled(cron = "0 15 3 * * ?", zone = "Asia/Kathmandu")
+@Scheduled(cron = "59 9 * * * ?", zone = "Asia/Kathmandu")
     public void run() {
         LocalDate today = LocalDate.now();
-        String jobName = "INTEREST_CALCULATION";
+        String jobName = "INTERESTS_CALCULATION";
+
         boolean alreadyRun = jobLogRepository.existsByJobNameAndRunDate(jobName, today);
         if (alreadyRun) {
-            log.info("Daily accrual calculated already {}", today);
-            return; // Skip execution
+            log.info("Daily accrual already calculated for {}", today);
+           return; // Skip if already executed today
         }
 
-        log.info("Daily accrual job triggered at {}", LocalDate.now());
+        log.info("Daily accrual job triggered at {}", today);
+
         List<LoanDetails> loans = loanRepo.findByStatusIn(List.of("ACTIVE", "OVERDUE", "DEFAULT"));
         for (LoanDetails loan : loans) {
             List<EmiSchedule> schedules = emiRepo.findEmiScheduleByLoanNumber(loan.getLoanNumber());
             for (EmiSchedule s : schedules) {
                 if (Boolean.TRUE.equals(s.getLastInstallment())) continue;
+
                 long daysDiff = ChronoUnit.DAYS.between(s.getEmiStartDate(), s.getEmiDate());
                 if (daysDiff > 0) {
                     s.setStatus("ACTIVE");
@@ -54,27 +51,31 @@ public class DailyAccurals {
                     loanRepo.save(loan);
                 }
 
-                if (s.getEmiDate().isBefore(today)
-                        && !"OVERDUE".equalsIgnoreCase(s.getStatus())) {
+                if (s.getEmiDate().isBefore(today) && !"OVERDUE".equalsIgnoreCase(s.getStatus())) {
                     s.setStatus("OVERDUE");
                     loan.setStatus("OVERDUE");
                     emiRepo.save(s);
                     loanRepo.save(loan);
                 }
+
                 LocalDate emiStart = s.getEmiStartDate();
                 double remainingPrincipal = s.getRemainingAmount();
                 double annualRate = loan.getInterestRate();
                 double dailyInterest = (remainingPrincipal * annualRate) / (100 * DAYS_IN_YEAR);
                 dailyInterest = round(dailyInterest);
+
                 accuralsService.saveInterest(
                         loan.getLoanNumber(), s.getId(), today, dailyInterest, s.getEmiMonth()
                 );
 
-                if (!today.isBefore(emiStart) && !"PAID".equalsIgnoreCase(s.getStatus()) && !"OVERDUE".equalsIgnoreCase(s.getStatus())) {
+                if (!today.isBefore(emiStart)
+                        && !"PAID".equalsIgnoreCase(s.getStatus())
+                        && !"OVERDUE".equalsIgnoreCase(s.getStatus())) {
                     s.setStatus("ACTIVE");
                     emiRepo.save(s);
                     loanRepo.save(loan);
                 }
+
                 double paidAmount = (s.getPaidAmount() == null) ? 0.0 : s.getPaidAmount();
                 double unpaidAmount = s.getEmiAmount() - paidAmount;
 
@@ -112,11 +113,12 @@ public class DailyAccurals {
                         loan.setStatus("DEFAULT");
                         emiRepo.save(s);
                         loanRepo.save(loan);
-                        jobLogRepository.save(new DailyJobLog(today,jobName,"SUCCESS"));
                     }
                 }
             }
-        } log.info("Interest calculated at {}", today);
+        }
+        jobLogRepository.save(new DailyJobLog(today, jobName, "SUCCESS"));
+        log.info("Interest calculation completed for {}", today);
     }
 
     private double round(double value) {
